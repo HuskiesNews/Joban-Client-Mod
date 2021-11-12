662584c032ffe05bcbf543d3983ba5b7d24feb83
LX862 <kenny.mh.hui@gmail.com>
2021-11-13 02:26:26 +0800

    Station Name (standing)

--- src/main/java/com/lx/jsblock/JoestuClient.java
+++ src/main/java/com/lx/jsblock/JoestuClient.java
@@ -1,6 +1,7 @@
 package com.lx.jsblock;
 
 import com.lx.jsblock.render.RenderSignalLight;
+import com.lx.jsblock.render.RenderStationNameTall;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendereregistry.v1.BlockEntityRendererRegistry;
@@ -15,13 +16,14 @@ public class JoestuClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(Blocks.HELPLINE_1, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(Blocks.HELPLINE_2, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(Blocks.HELPLINE_3, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(Blocks.STATION_NAME_TALL_STAND, RenderLayer.getCutout());
         BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_RED_ENTITY_1, dispatcher -> new RenderSignalLight<>(dispatcher, true, 0xFFFF0000, false,false, 0xFF0000FF));
         BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_RED_ENTITY_2, dispatcher -> new RenderSignalLight<>(dispatcher, true, 0xFFFF0000, false,true, 0xFF0000FF));
         BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_BLUE_ENTITY, dispatcher -> new RenderSignalLight<>(dispatcher, true, 0xFF0000FF, false, false,0xFF0000FF));
         BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_GREEN_ENTITY, dispatcher -> new RenderSignalLight<>(dispatcher, true, 0xFF00FF00, false,false,0xFF0000FF));
         BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_INVERTED_ENTITY_1, dispatcher -> new RenderSignalLight<>(dispatcher, true, 0, true,true,0xFF0000FF));
         BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_INVERTED_ENTITY_2, dispatcher -> new RenderSignalLight<>(dispatcher, true, 0, true,false,0xFF00FF00));
-        BlockEntityRendererRegistry.INSTANCE.register(Blocks.STATION_NAME_TALL_STAND_TILE_ENTITY, mtr.render.RenderStationNameTall::new);
+        BlockEntityRendererRegistry.INSTANCE.register(Blocks.STATION_NAME_TALL_STAND_TILE_ENTITY, RenderStationNameTall::new);
         registerStationColor(Blocks.STATION_NAME_TALL_STAND);
 
     }
--- src/main/java/com/lx/jsblock/blocks/StationNameTallStand.java
+++ src/main/java/com/lx/jsblock/blocks/StationNameTallStand.java
@@ -17,7 +17,7 @@ public class StationNameTallStand extends mtr.block.BlockStationNameTallBase {
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         final Pair<Integer, Integer> bounds = getBounds(state);
-        return IBlock.getVoxelShapeByDirection(2, bounds.getLeft(), 0, 14, bounds.getRight(), 0.5, IBlock.getStatePropertySafe(state, FACING));
+        return IBlock.getVoxelShapeByDirection(1, bounds.getLeft(), 0, 15, bounds.getRight(), 0.5, IBlock.getStatePropertySafe(state, FACING));
     }
 
     @Override
@@ -32,10 +32,29 @@ public class StationNameTallStand extends mtr.block.BlockStationNameTallBase {
         return new TileEntityStationNameTallStand();
     }
 
-    public static class TileEntityStationNameTallStand extends  mtr.block.BlockStationNameTallWall.TileEntityStationNameTallBase {
-
+    public static class TileEntityStationNameTallStand extends mtr.block.BlockStationNameTallWall.TileEntityStationNameTallBase {
         public TileEntityStationNameTallStand() {
-            super(Blocks.STATION_NAME_TALL_STAND_TILE_ENTITY, 0.03125F);
+            super(Blocks.STATION_NAME_TALL_STAND_TILE_ENTITY, 0.04025F);
         }
+    }
+
+    public static Pair<Integer, Integer> getBounds(BlockState state) {
+        final EnumThird third = IBlock.getStatePropertySafe(state, THIRD);
+        final int start, end;
+        switch (third) {
+            case LOWER:
+                start = 0;
+                end = 16;
+                break;
+            case UPPER:
+                start = 0;
+                end = 6;
+                break;
+            default:
+                start = 0;
+                end = 16;
+                break;
+        }
+        return new Pair<>(start, end);
     }
 }
