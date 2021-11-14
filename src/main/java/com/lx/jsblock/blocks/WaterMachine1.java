8c50d501d279e311cf2c0021f920d91c202ad1af
LX862 <kenny.mh.hui@gmail.com>
2021-11-15 00:44:13 +0800

    Added Functionalities for Water Machine

--- src/main/java/com/lx/jsblock/blocks/WaterMachine1.java
+++ src/main/java/com/lx/jsblock/blocks/WaterMachine1.java
@@ -3,12 +3,20 @@ import mtr.block.IBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
-import net.minecraft.block.enums.DoubleBlockHalf;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.potion.PotionUtil;
+import net.minecraft.potion.Potions;
 import net.minecraft.state.StateManager;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 public class WaterMachine1 extends mtr.block.BlockDirectionalDoubleBlockBase {
     public WaterMachine1(Settings settings) {
@@ -18,7 +26,18 @@ public class WaterMachine1 extends mtr.block.BlockDirectionalDoubleBlockBase {
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
         final Direction facing = IBlock.getStatePropertySafe(state, FACING);
-            return IBlock.getVoxelShapeByDirection(2.5, 0, 0, 13.5, 16, 11, facing);
+        return IBlock.getVoxelShapeByDirection(2.5, 0, 0, 13.5, 16, 11, facing);
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        for(ItemStack itm : player.getItemsHand()) {
+            if(itm.getItem().asItem().equals(Items.GLASS_BOTTLE)) {
+                player.setStackInHand(hand, PotionUtil.setPotion(new ItemStack(Items.POTION), Potions.WATER));
+                return ActionResult.SUCCESS;
+            }
+        }
+        return ActionResult.FAIL;
     }
 
     @Override
