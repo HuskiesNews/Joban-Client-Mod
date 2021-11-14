117326714c3b937b2db5a6e06afa33e97d3034e6
LX862 <kenny.mh.hui@gmail.com>
2021-11-15 01:56:00 +0800

    Make constant signal light works anywhere

--- src/main/java/com/lx/jsblock/JoestuClient.java
+++ src/main/java/com/lx/jsblock/JoestuClient.java
@@ -1,5 +1,6 @@
 package com.lx.jsblock;
 
+import com.lx.jsblock.render.RenderConstantSignalLight;
 import com.lx.jsblock.render.RenderSignalLight;
 import com.lx.jsblock.render.RenderStationNameTall;
 import net.fabricmc.api.ClientModInitializer;
@@ -19,12 +20,12 @@ public class JoestuClient implements ClientModInitializer {
         BlockRenderLayerMap.INSTANCE.putBlock(Blocks.WATER_MACHINE_1, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(Blocks.CEILING_1, RenderLayer.getCutout());
         BlockRenderLayerMap.INSTANCE.putBlock(Blocks.STATION_NAME_TALL_STAND, RenderLayer.getCutout());
-        BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_RED_ENTITY_1, dispatcher -> new RenderSignalLight<>(dispatcher, true, 0xFFFF0000, false,false, 0xFF0000FF));
-        BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_RED_ENTITY_2, dispatcher -> new RenderSignalLight<>(dispatcher, true, 0xFFFF0000, false,true, 0xFF0000FF));
-        BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_BLUE_ENTITY, dispatcher -> new RenderSignalLight<>(dispatcher, true, 0xFF0000FF, false, false,0xFF0000FF));
-        BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_GREEN_ENTITY, dispatcher -> new RenderSignalLight<>(dispatcher, true, 0xFF00FF00, false,false,0xFF0000FF));
-        BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_INVERTED_ENTITY_1, dispatcher -> new RenderSignalLight<>(dispatcher, true, 0, true,true,0xFF0000FF));
-        BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_INVERTED_ENTITY_2, dispatcher -> new RenderSignalLight<>(dispatcher, true, 0, true,false,0xFF00FF00));
+        BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_RED_ENTITY_1, dispatcher -> new RenderConstantSignalLight<>(dispatcher, true, 0xFFFF0000, false));
+        BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_RED_ENTITY_2, dispatcher -> new RenderConstantSignalLight<>(dispatcher, true, 0xFFFF0000, true));
+        BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_BLUE_ENTITY, dispatcher -> new RenderConstantSignalLight<>(dispatcher, true, 0xFF0000FF, false));
+        BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_GREEN_ENTITY, dispatcher -> new RenderConstantSignalLight<>(dispatcher, true, 0xFF00FF00,false));
+        BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_INVERTED_ENTITY_1, dispatcher -> new RenderSignalLight<>(dispatcher, true, true,true, 0xFF0000FF));
+        BlockEntityRendererRegistry.INSTANCE.register(Blocks.SIGNAL_LIGHT_INVERTED_ENTITY_2, dispatcher -> new RenderSignalLight<>(dispatcher, true, true,false,0xFF00FF00));
         BlockEntityRendererRegistry.INSTANCE.register(Blocks.STATION_NAME_TALL_STAND_TILE_ENTITY, RenderStationNameTall::new);
         registerStationColor(Blocks.STATION_NAME_TALL_STAND);
     }
--- src/main/java/com/lx/jsblock/render/RenderSignalLight.java
+++ src/main/java/com/lx/jsblock/render/RenderSignalLight.java
@@ -11,28 +11,23 @@ import net.minecraft.util.math.Direction;
 public class RenderSignalLight<T extends BlockEntity> extends mtr.render.RenderSignalBase<T> {
 
         final int proceedColor;
-        final int constantColor;
         final boolean inverted;
-        final int colorRed = 0xFFFF0000;
         final boolean redOnTop;
 
-        public RenderSignalLight(BlockEntityRenderDispatcher dispatcher, boolean isSingleSided, int constantColor, boolean inverted, boolean redOnTop, int proceedColor) {
+        public RenderSignalLight(BlockEntityRenderDispatcher dispatcher, boolean isSingleSided, boolean inverted, boolean redOnTop, int proceedColor) {
             super(dispatcher, isSingleSided);
             this.proceedColor = proceedColor;
-            this.constantColor = constantColor;
             this.redOnTop = redOnTop;
             this.inverted = inverted;
         }
 
         @Override
         protected void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, VertexConsumer vertexConsumer, T entity, float tickDelta, Direction facing, boolean isOccupied, boolean isBackSide) {
-                if(inverted) {
-                    float y = isOccupied == redOnTop ? 0.4375F : 0.0625F;
-                    IDrawing.drawTexture(matrices, vertexConsumer, -0.125F, y, -0.19375F, 0.125F, y + 0.25F, -0.19375F, facing.getOpposite(), isOccupied ? proceedColor : colorRed, MAX_LIGHT_GLOWING);
-                } else {
-                    /* Repurposed to if the corresponding light is on top in this block */
-                    float y = redOnTop ? 0.4375F : 0.0625F;
-                    IDrawing.drawTexture(matrices, vertexConsumer, -0.125F, y, -0.19375F, 0.125F, y + 0.25F, -0.19375F, facing.getOpposite(), constantColor, MAX_LIGHT_GLOWING);
-                }
+            float y = isOccupied == redOnTop ? 0.4375F : 0.0625F;
+            if(inverted) {
+                IDrawing.drawTexture(matrices, vertexConsumer, -0.125F, y, -0.19375F, 0.125F, y + 0.25F, -0.19375F, facing.getOpposite(), isOccupied ? proceedColor : 0xFFFF0000, MAX_LIGHT_GLOWING);
+            } else {
+                IDrawing.drawTexture(matrices, vertexConsumer, -0.125F, y, -0.19375F, 0.125F, y + 0.25F, -0.19375F, facing.getOpposite(), isOccupied ? 0xFFFF0000 : proceedColor, MAX_LIGHT_GLOWING);
             }
         }
+}
