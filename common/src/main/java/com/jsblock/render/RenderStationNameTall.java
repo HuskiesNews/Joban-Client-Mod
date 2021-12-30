e031764bf62333149d764bf270b6c11428d767fe
LX862 <kenny.mh.hui@gmail.com>
2021-12-30 23:33:58 +0800

    Fix overflowing on standing station sign

--- common/src/main/java/com/jsblock/render/RenderStationNameTall.java
+++ common/src/main/java/com/jsblock/render/RenderStationNameTall.java
@@ -18,7 +18,7 @@ public class RenderStationNameTall<T extends BlockStationNameTallBase.TileEntity
 
 	@Override
 	protected void drawStationName(BlockStationNameBase.TileEntityStationNameBase entity, PoseStack matrices, MultiBufferSource vertexConsumers, MultiBufferSource.BufferSource immediate, String stationName, int color, int light) {
-		IDrawing.drawStringWithFont(matrices, Minecraft.getInstance().font, immediate, IGui.formatVerticalChinese(stationName), HorizontalAlignment.CENTER, VerticalAlignment.CENTER, 0, -0.12F, 0.75F, 1.5F, 80, color, false, light, null);
+		IDrawing.drawStringWithFont(matrices, Minecraft.getInstance().font, immediate, IGui.formatVerticalChinese(stationName), HorizontalAlignment.CENTER, VerticalAlignment.CENTER, 0, -0.12F, 0.73F, 1.0F, 80, color, false, light, null);
 	}
 }
 
