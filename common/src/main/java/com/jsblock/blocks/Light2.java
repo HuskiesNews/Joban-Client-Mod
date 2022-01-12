d44ac11f1636e94d233e669a6fe14af409afcfd6
LX862 <kenny.mh.hui@gmail.com>
2022-01-12 20:30:12 +0800

    Make ceiling light placable on ground

--- common/src/main/java/com/jsblock/blocks/Light2.java
+++ common/src/main/java/com/jsblock/blocks/Light2.java
@@ -6,18 +6,24 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class Light2 extends Block {
+	public static final BooleanProperty CEILING = BooleanProperty.create("ceiling");
+
 	public Light2(Properties settings) {
 		super(settings);
+		registerDefaultState(defaultBlockState().setValue(CEILING, true));
 	}
+
 
 	@Override
 	public VoxelShape getShape(BlockState state, BlockGetter blockGetter, BlockPos pos, CollisionContext collisionContext) {
-		return Block.box(4, 15.9, 4, 12, 16, 12);
+		return state.getValue(CEILING) ? Block.box(4, 15.9, 4, 12, 16, 12) : Block.box(4, 0, 4, 12, 0.1, 12);
 	}
 
 	@Override
@@ -26,8 +32,18 @@ public class Light2 extends Block {
 	}
 
 	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(CEILING);
+	}
+
+	@Override
 	public BlockState getStateForPlacement(BlockPlaceContext ctx) {
 		BlockState blockAbove = ctx.getLevel().getBlockState(ctx.getClickedPos().above());
-		return blockAbove.getBlock().equals(Blocks.AIR) ? null : defaultBlockState();
+		BlockState blockBelow = ctx.getLevel().getBlockState(ctx.getClickedPos().below());
+		if (blockAbove.getBlock().equals(Blocks.AIR)) {
+ 			return blockBelow.getBlock().equals(Blocks.AIR) ? null : defaultBlockState().setValue(CEILING, false);
+		} else {
+			return defaultBlockState().setValue(CEILING, true);
+		}
 	}
 }
