f6799c4feae5876829966cd00b36d286753d9bd7
StrikeSNC <40461728+StrikeSNC@users.noreply.github.com>
2022-01-17 19:52:16 +0800

    Revert "Revert "Update TicketBarrier1Decor.java""
    This reverts commit 4d7e8cabbdb660eaa4115b9644159a3cb0883d61.

--- common/src/main/java/com/jsblock/blocks/TicketBarrier1Decor.java
+++ common/src/main/java/com/jsblock/blocks/TicketBarrier1Decor.java
@@ -1,25 +1,17 @@
 package com.jsblock.blocks;
 
-import mtr.SoundEvents;
 import mtr.block.IBlock;
-import mtr.data.TicketSystem;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
-import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 
@@ -37,7 +29,7 @@ public class TicketBarrier1Decor extends HorizontalDirectionalBlock {
 	@Override
 	public VoxelShape getShape(BlockState state, BlockGetter blockGetter, BlockPos pos, CollisionContext collisionContext) {
 		final Direction facing = IBlock.getStatePropertySafe(state, FACING);
-		return IBlock.getVoxelShapeByDirection(12, 0, 0, 16, 15, 16, facing);
+		return IBlock.getVoxelShapeByDirection(12, 0, 0, 16, 24, 16, facing);
 	}
 
 	@Override
