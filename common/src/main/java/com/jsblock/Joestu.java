570a99d5bd7dcd545a81aa6b4a516fa5583bd348
LX862 <kenny.mh.hui@gmail.com>
2022-01-25 19:04:48 +0800

    Bump versions, Fix licensing

--- common/src/main/java/com/jsblock/Joestu.java
+++ common/src/main/java/com/jsblock/Joestu.java
@@ -11,7 +11,7 @@ import java.util.function.BiConsumer;
 
 public class Joestu {
 	public static final String MOD_ID = "jsblock";
-	public static final String VERSION = "1.0.5";
+	public static final String VERSION = "1.0.6";
 	public static final Logger LOGGER = LogManager.getLogger("Joestu Client");
 
 	public static void init(
