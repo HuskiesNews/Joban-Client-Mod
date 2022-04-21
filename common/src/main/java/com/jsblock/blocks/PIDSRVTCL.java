2bf9e8b8672be2950708ef116c878965f406753e
LX862 <kenny.mh.hui@gmail.com>
2022-04-21 21:45:48 +0800

    Fix RV PIDS Shape

--- common/src/main/java/com/jsblock/blocks/PIDSRVTCL.java
+++ common/src/main/java/com/jsblock/blocks/PIDSRVTCL.java
@@ -1,11 +1,23 @@
 package com.jsblock.blocks;
 
 import com.jsblock.BlockEntityTypes;
+import mtr.block.IBlock;
 import mtr.mappings.BlockEntityMapper;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class PIDSRVTCL extends PIDSRVBase {
+
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter blockGetter, BlockPos pos, CollisionContext collisionContext) {
+		VoxelShape shape1 = IBlock.getVoxelShapeByDirection(5.9, -3.0, 0, 10.1, 11, 12, IBlock.getStatePropertySafe(state, FACING));
+		VoxelShape shape2 = IBlock.getVoxelShapeByDirection(7.5, 11, 8.5, 8.5, 16, 9.5, IBlock.getStatePropertySafe(state, FACING));
+		return Shapes.or(shape1, shape2);
+	}
 
 	@Override
 	public BlockEntityMapper createBlockEntity(BlockPos pos, BlockState state) {
